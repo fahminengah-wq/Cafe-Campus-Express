829e9a90e04db7d6ce777dbe7a4d20c0246c79c8
fahminengah-wq <fahminengah@gmail.com>
2026-01-20 23:44:30 +0800

    latest

--- api/lambda.php
+++ api/lambda.php
@@ -5,6 +5,35 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Force MySQL connection if DB credentials are set
+if (!empty(getenv('DB_HOST'))) {
+    $_ENV['DB_CONNECTION'] = 'mysql';
+}
+
+// Configure writable directories for Vercel serverless environment
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/storage/framework/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/storage/framework/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/framework/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/framework/cache/routes-v7.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/storage/framework/cache/events.php';
+$_ENV['SESSION_FILES_PATH'] = '/tmp/storage/framework/sessions';
+$_ENV['LOG_PATH'] = '/tmp/storage/logs/laravel.log';
+
+// Create necessary directories in /tmp
+if (!file_exists('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+if (!file_exists('/tmp/storage/framework/cache')) {
+    mkdir('/tmp/storage/framework/cache', 0755, true);
+}
+if (!file_exists('/tmp/storage/framework/sessions')) {
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+}
+if (!file_exists('/tmp/storage/logs')) {
+    mkdir('/tmp/storage/logs', 0755, true);
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
